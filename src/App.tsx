1f987d0f69957c950ff154138777f0f20bebe71b
Dmitry Limansky <DIMCH.L@mail.ru>
2023-02-27 02:30:44 +0300

    Added basic structure and routing

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Router from 'components/router/Router';
+import './App.scss';
+
+const App = () =>  {
+  return (
+      <Router/>
+  );
+}
+
+export default App;
